31da148070d13d1fbd83d1a026586d88ba71ac31
Taiwo <taiwooluwafemi654@gmail.com>
2025-07-14 14:05:06 +0100

    Add script to print first argument or default message

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,9 +1,12 @@
- #!/usr/bin/node
+#!/usr/bin/node
 
-const arg = process.argv[2];
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
 
-if (arg === undefined) {
+if (arg1 === undefined) {
   console.log('No argument');
+} else if (arg2 === undefined) {
+  console.log(arg1);
 } else {
-  console.log(arg);
+  console.log(`${arg1} ${arg2}`);
 }
